a8438f2777fa0426fa5361ac44b84a11bf83aebd
alocnayr <alocnayr101@gmail.com>
2023-11-30 11:30:59 -0500

    Dummy Compliance Data Public Not Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <h1>Hello World</h1>;
-}
+// App.jsx
+
+import React from 'react';
+import ComplianceHistoryChart from './ComplianceHistoryChart';
+import complianceData from 'Dummy Compliance Data.csv';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Compliance History Chart</h1>
+      <ComplianceHistoryChart data={complianceData} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/ComplianceHistoryChart.jsx
@@ -0,0 +1,52 @@
+// ComplianceHistoryChart.js
+
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+
+const ComplianceHistoryChart = ({ data }) => {
+  const chartRef = useRef();
+
+  useEffect(() => {
+    const svg = d3.select(chartRef.current);
+    const parseDate = d3.timeParse('%Y');
+
+    // Parse CSV data
+    const parsedData = d3.csvParse(data, (d) => ({
+      Date: parseDate(d.Date),
+      ComplianceStatus: d['Compliance Status'],
+    }));
+
+    // Group data by year
+    const dataByYear = d3.group(parsedData, (d) => d.Date.getFullYear());
+
+    // D3.js logic to create a bar chart
+    const width = 600;
+    const height = 300;
+
+    const xScale = d3
+      .scaleBand()
+      .domain(Array.from(dataByYear.keys()))
+      .range([0, width])
+      .padding(0.1);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(dataByYear, (d) => d[1].length)])
+      .range([height, 0]);
+
+    svg.selectAll('rect')
+      .data(dataByYear)
+      .enter()
+      .append('rect')
+      .attr('x', (d) => xScale(d[0]))
+      .attr('y', (d) => yScale(d[1].length))
+      .attr('width', xScale.bandwidth())
+      .attr('height', (d) => height - yScale(d[1].length))
+      .attr('fill', 'steelblue');
+
+  }, [data]);
+
+  return <svg ref={chartRef}></svg>;
+};
+
+export default ComplianceHistoryChart;
