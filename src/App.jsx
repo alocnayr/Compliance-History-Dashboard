1b9c7a25413465e9cf9090c952670975770af510
whuang214 <whuang2146@gmail.com>
2023-11-30 12:05:12 -0500

    add csv parsing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-// App.jsx
-
-import React from 'react';
-import ComplianceHistoryChart from './ComplianceHistoryChart';
-import complianceData from 'Dummy Compliance Data.csv';
+import React, { useState, useEffect } from "react";
+import ComplianceHistoryChart from "./ComplianceHistoryChart";
+import Papa from "papaparse";
 
 const App = () => {
+  const [complianceData, setComplianceData] = useState([]);
+
+  useEffect(() => {
+    fetch("/Dummy Compliance Data.csv")
+      .then((response) => response.text())
+      .then((csvText) => {
+        // Parse the CSV data with PapaParse
+        Papa.parse(csvText, {
+          header: true, // Assumes the first row of your CSV are headers
+          dynamicTyping: true, // Automatically converts strings to numbers, booleans, etc.
+          complete: (results) => {
+            setComplianceData(results.data);
+            console.log("Updated data:", results.data); // Log the updated data
+          },
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching the CSV file:", error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Compliance History Chart</h1>
--- src/ComplianceHistoryChart.jsx
+++ src/ComplianceHistoryChart.jsx
@@ -1,19 +1,19 @@
 // ComplianceHistoryChart.js
 
-import React, { useEffect, useRef } from 'react';
-import * as d3 from 'd3';
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
 
 const ComplianceHistoryChart = ({ data }) => {
   const chartRef = useRef();
 
   useEffect(() => {
     const svg = d3.select(chartRef.current);
-    const parseDate = d3.timeParse('%Y');
+    const parseDate = d3.timeParse("%Y");
 
     // Parse CSV data
     const parsedData = d3.csvParse(data, (d) => ({
       Date: parseDate(d.Date),
-      ComplianceStatus: d['Compliance Status'],
+      ComplianceStatus: d["Compliance Status"],
     }));
 
     // Group data by year
@@ -34,16 +34,16 @@ const ComplianceHistoryChart = ({ data }) => {
       .domain([0, d3.max(dataByYear, (d) => d[1].length)])
       .range([height, 0]);
 
-    svg.selectAll('rect')
+    svg
+      .selectAll("rect")
       .data(dataByYear)
       .enter()
-      .append('rect')
-      .attr('x', (d) => xScale(d[0]))
-      .attr('y', (d) => yScale(d[1].length))
-      .attr('width', xScale.bandwidth())
-      .attr('height', (d) => height - yScale(d[1].length))
-      .attr('fill', 'steelblue');
-
+      .append("rect")
+      .attr("x", (d) => xScale(d[0]))
+      .attr("y", (d) => yScale(d[1].length))
+      .attr("width", xScale.bandwidth())
+      .attr("height", (d) => height - yScale(d[1].length))
+      .attr("fill", "steelblue");
   }, [data]);
 
   return <svg ref={chartRef}></svg>;
