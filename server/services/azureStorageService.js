6166b699105b9a5134bb445687d7108f64391591
whuang214 <whuang2146@gmail.com>
2023-12-06 13:41:37 -0500

    add try catch to service

--- server/services/azureStorageService.js
+++ server/services/azureStorageService.js
@@ -1,16 +1,23 @@
-// services/azureStorageService.js
 const { BlobServiceClient } = require("@azure/storage-blob");
 
-// services/azureStorageService.js
 const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
 const containerName = "compliance-reports"; // Container name can also be stored in an environment variable
 
 exports.uploadPdf = async (pdfData, blobName) => {
-  const blobServiceClient =
-    BlobServiceClient.fromConnectionString(connectionString);
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+  try {
+    const blobServiceClient =
+      BlobServiceClient.fromConnectionString(connectionString);
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-  const buffer = Buffer.from(pdfData, "base64");
-  await blockBlobClient.upload(buffer, buffer.length);
+    const buffer = Buffer.from(pdfData, "base64");
+    await blockBlobClient.upload(buffer, buffer.length);
+
+    // If the upload is successful, you can add additional logic here
+    console.log(`File ${blobName} uploaded successfully.`);
+  } catch (error) {
+    // Handle the error here
+    console.error("Error occurred during file upload:", error);
+    throw error; // Re-throw the error if you want the calling function to handle it as well
+  }
 };
